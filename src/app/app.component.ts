0e931098bbc939674cb5fa4f53c388e15522c242
radu <radualexandrubulai@gmail.com>
2022-10-12 01:08:10 +0300

    Implement URL Parser logic using uRL JS APi, build new URL with new Search Query

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,59 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   title = 'url-parser-and-builder-app';
+  public url!: URL;
+  public urlParams!: any;
+  public urlNew: any;
+  public urlNewParams!: any;
+  public urlNewComplete!: string;
+
+  onClearUrl() {
+    delete this.urlNew;
+    this.urlNewComplete = "";
+  }
 
   onParseUrl(urlForm: NgForm) {
-    console.log(urlForm.value);
+    const methodName = "onParseUrl()";
+    
+    try {
+      this.url = new URL(urlForm.value.url);
+      this.urlNew = new URL(urlForm.value.url);
+
+      const urlSearchParams = new URLSearchParams(this.url.search);
+      this.urlParams = Object.fromEntries(urlSearchParams.entries());
+      this.urlNewParams = Object.fromEntries(urlSearchParams.entries());
+      this.urlNewComplete = "";
+      
+      console.log(this.url);
+      console.log(this.urlParams);
+
+    } catch (error) {
+      console.error(`${methodName} ${urlForm.value.url} is not a valid URL - ${error}`);
+    }
+  }
+
+  onBuildUrl(urlNewForm: NgForm) {
+    const methodName = "onBuildUrl()";
+    console.log(urlNewForm.value);
+    try {
+      this.urlNew = new URL(urlNewForm.value.href);
+      this.urlNewComplete = urlNewForm.value.href;
+      console.log(this.urlNew);
+
+    } catch (error) {
+      console.error(`${methodName} ${urlNewForm.value.url} is not a valid URL - ${error}`);
+    }
+  }
+
+  onBuildUrlParams(urlNewParamsForm: NgForm) {
+    let urlNewSearchParams = new URLSearchParams();
+    for (const [key, value] of Object.entries(urlNewParamsForm.value)) {
+      console.log(key, value);
+      urlNewSearchParams.append(key, value as string);
+    }
+    console.log(urlNewSearchParams.toString());
+    this.urlNewParams = urlNewSearchParams;
+    this.urlNew.search = this.urlNewParams.toString();
+    this.urlNewComplete = this.urlNew.href;
   }
 }
